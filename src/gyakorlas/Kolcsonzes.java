361352d11abed92a645bd0b7a62a664630b758dc
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-04-30 22:06:22 +0200

    create class file

---
+++ src/gyakorlas/Kolcsonzes.java
@@ -0,0 +1,24 @@
+package gyakorlas;
+
+public class Kolcsonzes {
+
+	private String nev;
+	private char jarmuAzonosito;
+	private int takeHour;
+	private int takeMinute;
+	private int comeHour;
+	private int comeMinute;
+	
+	public Kolcsonzes(String nev, char jarmuAzonosito, int takeHour, int takeMinute, int comeHour, int comeMinute) {
+		super();
+		this.nev = nev;
+		this.jarmuAzonosito = jarmuAzonosito;
+		this.takeHour = takeHour;
+		this.takeMinute = takeMinute;
+		this.comeHour = comeHour;
+		this.comeMinute = comeMinute;
+	}
+	
+	
+	
+}
